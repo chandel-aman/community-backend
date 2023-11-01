7dd6244d3d86fb87639880c2d0683fe2706745db
chandel-aman <chandel.s.aman@gmail.com>
2023-11-01 23:43:10 +0530

    fix

--- src/socket.js
+++ src/socket.js
@@ -1,41 +1,41 @@
-// Import necessary libraries
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+// // Import necessary libraries
+// const express = require('express');
+// const http = require('http');
+// const socketIo = require('socket.io');
 
-// Set up express
-const app = express();
-const server = http.createServer(app);
+// // Set up express
+// const app = express();
+// const server = http.createServer(app);
 
-// Set up Socket.IO
-const io = socketIo(server);
+// // Set up Socket.IO
+// const io = socketIo(server);
 
-// Listen for socket connections
-io.on('connection', (socket) => {
-  console.log('New user connected');
+// // Listen for socket connections
+// io.on('connection', (socket) => {
+//   console.log('New user connected');
 
-  // When a new message is received
-  socket.on('message', async (data) => {
-    console.log('Received new message:', data);
+//   // When a new message is received
+//   socket.on('message', async (data) => {
+//     console.log('Received new message:', data);
 
-    // Broadcast the new message to everyone in the chat room
-    io.to(data.chatId).emit('message', data);
-  });
+//     // Broadcast the new message to everyone in the chat room
+//     io.to(data.chatId).emit('message', data);
+//   });
 
-  // Listen for join room event
-  socket.on('join', (room) => {
-    console.log('User joined room:', room);
-    socket.join(room);
-  });
+//   // Listen for join room event
+//   socket.on('join', (room) => {
+//     console.log('User joined room:', room);
+//     socket.join(room);
+//   });
 
-  // Listen for disconnect event
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
+//   // Listen for disconnect event
+//   socket.on('disconnect', () => {
+//     console.log('User disconnected');
+//   });
+// });
 
-// Start the server
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// // Start the server
+// const PORT = 3000;
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
